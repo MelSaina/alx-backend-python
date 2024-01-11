f2515a4ba2ffc83dcd026e0573ba219e4bc7669c
MelSaina <gabbysiana@gmail.com>
2024-01-11 10:22:36 +0000

    0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Type annoted function add """
+
+def add ( a:float, b:float ) -> float:
+    """ this function adds to floats and reeturrns the sum as a float"""
+    return a + b
